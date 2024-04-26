9f4ef265431ad4ed00d58e0bd96e83eda3300ad1
Adelina Vaitus <adelinavaitus@gmail.com>
2024-04-26 12:50:15 +0300

    fetched the genre array and displaying the genres for the selected movie

--- movie-project/src/components/MovieListComponent.js
+++ movie-project/src/components/MovieListComponent.js
@@ -1,13 +1,14 @@
-import React, {Component} from 'react';
-import {Media, Row, Col, Modal, ModalHeader, ModalBody, Card} from 'reactstrap';
+import React, { Component } from 'react';
+import { Media, Row, Col, Modal, ModalHeader, ModalBody, Card, ModalFooter } from 'reactstrap';
 
-class MovieList extends Component{
+class MovieList extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
             movies: [],
+            genres: [],
             isModalMovieOpen: false,
             selectedMovie: null
         };
@@ -15,17 +16,18 @@ class MovieList extends Component{
         this.toggleModalMovie = this.toggleModalMovie.bind(this);
     }
 
-    toggleModalMovie(movie){
+    toggleModalMovie(movie) {
         this.setState({
             isModalMovieOpen: !this.state.isModalMovieOpen,
             selectedMovie: movie
         });
     }
 
-    componentDidMount(){
+    componentDidMount() {
         const API_KEY = process.env.REACT_APP_API_KEY;
         const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`;
-        
+        const GENRE_URL = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`;
+
         fetch(API_URL)
             .then(response => response.json())
             .then(data => {
@@ -36,18 +38,28 @@ class MovieList extends Component{
                 console.error('Error fetching data from API:', error);
             });
 
+        fetch(GENRE_URL)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ genres: data.genres });
+                console.log(data.genres);
+            })
+            .catch(error => {
+                console.error('Error fetching genres from API:', error);
+            });
     }
 
-    render(){
+    render() {
 
         const movies = this.state.movies;
         const selectedMovie = this.state.selectedMovie;
+        const genres = this.state.genres;
 
         // Diveded the movies into groups of 4 
         const movieGroups = movies.reduce((resultMovieArray, item, index) => {
-            const groupIndex = Math.floor(index/4);
+            const groupIndex = Math.floor(index / 4);
 
-            if(!resultMovieArray[groupIndex]){
+            if (!resultMovieArray[groupIndex]) {
                 resultMovieArray[groupIndex] = [];
             }
 
@@ -57,50 +69,62 @@ class MovieList extends Component{
         }, []);
 
         // We render each chunk in a separate row
-        const movieRows = movieGroups.map((group, index)=> {
+        const movieRows = movieGroups.map((group, index) => {
             return (
                 <Row key={index} className='mt-4'>
                     {group.map(movie => (
-                    <Col key={movie.id} sm="3">
-                        <Media left middle>
-                            <Media onClick={() => this.toggleModalMovie(movie)} object src= {`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.original_title} />
-                        </Media>
-                        <Media body className='ml-3'>
-                            <Media heading>{movie.original_title}</Media>
-                            <p>{movie.overview}</p>
-                        </Media>
-                    </Col>
+                        <Col key={movie.id} sm="3">
+                            <Media left middle>
+                                <Media onClick={() => this.toggleModalMovie(movie)} object src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.original_title} />
+                            </Media>
+                            <Media body className='ml-3'>
+                                <Media heading>{movie.original_title}</Media>
+                                <p>{movie.overview}</p>
+                            </Media>
+                        </Col>
                     ))}
                 </Row>
             );
         });
 
-        return(
+        return (
             <div className="container">
                 <Media list>
                     {movieRows}
                 </Media>
 
                 <Modal isOpen={this.state.isModalMovieOpen}>
-                    <ModalHeader toggle={this.toggleModalMovie}>{selectedMovie ? selectedMovie.original_title: null}</ModalHeader>
-                    <ModalBody>
-                        <Row>
-                            <Col>
-                                <Media object src={`https://image.tmdb.org/t/p/w200${selectedMovie? selectedMovie.poster_path : null}`} alt={selectedMovie? selectedMovie.original_title:null} />
-                            </Col>
-                            <Col>
-                                <Row>
-                                    <Card>Genres: </Card>
-                                </Row>
-                                <Row>
-                                    <Card>Release date: {selectedMovie? selectedMovie.release_date : null}</Card>
-                                </Row>
-                                <Row>
-                                    <Card>Description: {selectedMovie? selectedMovie.overview : null}</Card>
-                                </Row>
-                            </Col>
-                        </Row>
-                    </ModalBody>
+                    {
+                        selectedMovie ? (
+                            <div>
+                                <ModalHeader toggle={this.toggleModalMovie}>{selectedMovie.original_title}</ModalHeader>
+                                <ModalBody>
+                                    <Row>
+                                        <Col>
+                                            <Media object src={`https://image.tmdb.org/t/p/w200${selectedMovie.poster_path}`} alt={selectedMovie.original_title} />
+                                        </Col>
+                                        <Col>
+                                            <Row>
+                                                <Card>Genres:
+                                                    {
+                                                        selectedMovie.genre_ids ? selectedMovie.genre_ids.map((genreId) => {
+                                                            const genre = genres.find(genre => genre.id === genreId);
+                                                            return (<p>{genre.name}</p>);
+                                                        }) : null
+                                                    }
+                                                </Card>
+                                            </Row>
+                                            <Row>
+                                                <Card>Release date: {selectedMovie.release_date}</Card>
+                                            </Row>
+                                            <Row>
+                                                <Card>Description: {selectedMovie.overview}</Card>
+                                            </Row>
+                                        </Col>
+                                    </Row>
+                                </ModalBody>
+                            </div>
+                        ) : null}
                 </Modal>
             </div>
         );
